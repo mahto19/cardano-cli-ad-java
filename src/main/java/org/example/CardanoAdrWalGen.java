e066ff9ccea661215cbdf3571b77a6169bdb0471
mahto19 <159007006+mahto19@users.noreply.github.com>
2024-03-08 17:57:23 +0530

    Delete src/main/java/org/example/CardanoAdrWalGen.java

--- src/main/java/org/example/CardanoAdrWalGen.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class CardanoAdrWalGen {
-    public static void seedPhrase() {
-        String path = "src/main/resources/bin/";
-        String addressPath = path + "cardano-address"; // Path to cardano-address executable
-
-    }
-}
